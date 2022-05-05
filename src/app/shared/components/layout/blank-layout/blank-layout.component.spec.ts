b905e6592973c56d263586d49e112ba3870c5d5e
HasaneenDev <33404473+HasaneenDev@users.noreply.github.com>
2022-05-05 06:23:27 +0300

    update to v 11

--- src/app/shared/components/layout/blank-layout/blank-layout.component.spec.ts
+++ src/app/shared/components/layout/blank-layout/blank-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlankLayoutComponent } from './blank-layout.component';
 
@@ -6,7 +6,7 @@ describe('BlankLayoutComponent', () => {
   let component: BlankLayoutComponent;
   let fixture: ComponentFixture<BlankLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlankLayoutComponent ]
     })
