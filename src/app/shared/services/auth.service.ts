5240768782d7c490fdb49ae01361387d2e6fe7cf
HasaneenDev <33404473+HasaneenDev@users.noreply.github.com>
2022-05-05 05:53:38 +0300

    add login layout

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { LocalStoreService } from './local-store.service';
+import { Injectable } from "@angular/core";
+import { LocalStoreService } from "./local-store.service";
 
-import { of } from "rxjs";
+import { of, throwError } from "rxjs";
 import { delay } from "rxjs/operators";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
   authenticated = true;
-  constructor(private store: LocalStoreService, private router: Router) { 
+  constructor(private store: LocalStoreService, private router: Router) {
     this.checkAuth();
   }
 
@@ -19,14 +18,27 @@ export class AuthService {
     this.authenticated = this.store.getItem("demo_login_status");
   }
   signin(credentials) {
-    this.authenticated = true;
-    this.store.setItem("demo_login_status", true);
-    return of({}).pipe(delay(1500));
+    // demo data
+    const dataInServer = {
+      email: "demo@has.com",
+      password: "12345678",
+    };
+    console.log("credentials : ", credentials);
+    // this step for backend proccess and I'm write here becuase return response (success or error)
+    if (
+      credentials.email === dataInServer.email &&
+      credentials.password === dataInServer.password
+    ) {
+      this.authenticated = true;
+      this.store.setItem("demo_login_status", true);
+      return of({ successMessage: "success" }).pipe(delay(500));
+    }
+    return throwError("error");
   }
 
   signout() {
     this.authenticated = false;
     this.store.setItem("demo_login_status", false);
-    this.router.navigateByUrl('/accounts/login')
+    this.router.navigateByUrl("/accounts/login");
   }
 }
